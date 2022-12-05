dd3b18326a913ff09ce0ee7a005aff028bf4adc4
Eric Dasse <christ.gououi@gmail.com>
2022-12-05 11:30:01 +0100

    `delivery.py`: add typing

--- delivery.py
+++ delivery.py
@@ -1,23 +1,23 @@
-class ToyMachine:
+class Sleigh:
     def __init__(self):
-        self.present_made = None
-
-    def make_present(self, present):
-        return present
-
-    def give_present(self, elf):
-        elf.carried_present = self.present_made
+        self.list_of_presents = []
 
 
 class Elf:
     def __init__(self):
         self.carried_present = None
 
-    def pack_into(self, sleigh):
+    def pack_into(self, sleigh: Sleigh):
         sleigh.list_of_presents.append(self.carried_present)
         self.carried_present = None
 
 
-class Sleigh:
+class ToyMachine:
     def __init__(self):
-        self.list_of_presents = []
+        self.present_made = None
+
+    def make_present(self, present: str) -> str:
+        return present
+
+    def give_present(self, elf: Elf):
+        elf.carried_present = self.present_made
