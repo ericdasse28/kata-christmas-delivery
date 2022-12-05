19cc186020a7fda3cc65d377f343aff596993666
Eric Dasse <christ.gououi@gmail.com>
2022-12-05 10:54:30 +0100

    Feat: Toy machine can give a present to an elf

--- delivery.py
+++ delivery.py
@@ -1,3 +1,14 @@
 class ToyMachine:
+    def __init__(self):
+        self.present_made = None
+
     def make_present(self, present):
         return present
+
+    def give_present(self, elf):
+        elf.current_present = self.present_made
+
+
+class Elf:
+    def __init__(self):
+        self.current_present = "present"
--- test_delivery.py
+++ test_delivery.py
@@ -1,6 +1,6 @@
 import pytest
 
-from delivery import ToyMachine
+from delivery import Elf, ToyMachine
 
 
 @pytest.mark.parametrize(
@@ -13,3 +13,23 @@ def test_toy_machine_can_make_a_present(present_to_make):
     present_made = toy_machine.make_present(present_to_make)
 
     assert present_made == present_to_make
+
+
+@pytest.mark.parametrize(
+    "present_made",
+    [
+        "Fullmetal album",
+        "Air Jordan shoes",
+        "Curry 8",
+        "Dragon Ball Xenoverse",
+        "Friends expo",
+    ],
+)
+def test_toy_machine_can_give_a_present_to_elf(present_made):
+    toy_machine = ToyMachine()
+    toy_machine.present_made = present_made
+    elf = Elf()
+
+    toy_machine.give_present(elf)
+
+    assert elf.current_present == toy_machine.present_made
